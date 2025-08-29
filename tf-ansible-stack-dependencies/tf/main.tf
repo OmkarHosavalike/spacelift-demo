ae228e4c4525a22c072b603d2eb52d0d65a1cdf2
OmkarHosavalike <58688067+OmkarHosavalike@users.noreply.github.com>
2025-08-29 16:03:28 +0530

    Update main.tf

--- tf-ansible-stack-dependencies/tf/main.tf
+++ tf-ansible-stack-dependencies/tf/main.tf
@@ -36,14 +36,6 @@ locals {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t2.micro"
     }
-    instance4 = {
-      ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
-    }
-    instance5 = {
-      ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
-    }
   }
 }
 
